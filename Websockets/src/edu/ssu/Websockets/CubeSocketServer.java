572cde5e85d40b522b853ae59886333ab0cbb172
Robert <Robert@TSUGUMORI>
2016-03-28 21:01:53 -0400

    Began working on Server

--- Websockets/src/edu/ssu/Websockets/CubeSocketServer.java
+++ Websockets/src/edu/ssu/Websockets/CubeSocketServer.java
@@ -5,11 +5,15 @@
  */
 package edu.ssu.Websockets;
 
+import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.java_websocket.WebSocket;
+import org.java_websocket.framing.Framedata;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 import sun.rmi.runtime.Log;
@@ -28,30 +32,65 @@ public class CubeSocketServer extends WebSocketServer{
     private ArrayList<WebSocket> clients;
     
     public CubeSocketServer() throws UnknownHostException{
-        super();
+        super(new InetSocketAddress(8080));
         this.clients = new ArrayList<>();
     }
     
     @Override
     public void onOpen(WebSocket ws, ClientHandshake ch) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        // What to do on client connect
     }
 
     @Override
     public void onClose(WebSocket ws, int i, String string, boolean bln) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        // what to do on client disconnect
     }
 
     @Override
     public void onMessage(WebSocket ws, String string) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //what to do on client message
+    }
+    
+    @Override
+    public void onMessage(WebSocket ws, ByteBuffer string) {
+        //what to do on client message
     }
 
     @Override
     public void onError(WebSocket ws, Exception excptn) {
+        // what to do on error
         System.out.println("Exception with " + ws.toString() + ".\n" + excptn.getMessage());
     }
     
+    @Override
+    public void onFragment(WebSocket conn, Framedata fragment)
+    {
+        // what to do on fragment receipt
+    }
+    
+    public void sendToAll(String message)
+    {
+        Collection<WebSocket> con = connections();
+        synchronized (con)
+        {
+            for(WebSocket c : con)
+            {
+                c.send(message);
+            }
+        }
+    }
+    
+    public void sendToAll(ByteBuffer buffer)
+    {
+        Collection<WebSocket> con = connections();
+        synchronized (con)
+        {
+            for(WebSocket c : con)
+            {
+                c.send(buffer);
+            }
+        }
+    }
     
     public static void main(String[] args)
     {
