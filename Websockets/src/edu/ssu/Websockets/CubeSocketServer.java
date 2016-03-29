ba5f9d9c62a4cc9af16a70aa5399d81bc9a461ca
Robert Appelmann <appelraptormann@gmail.com>
2016-03-29 16:01:27 -0400

    Working on it

--- Websockets/src/edu/ssu/Websockets/CubeSocketServer.java
+++ Websockets/src/edu/ssu/Websockets/CubeSocketServer.java
@@ -1,103 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.ssu.Websockets;
 
 import java.net.InetSocketAddress;
-import java.net.UnknownHostException;
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 import org.java_websocket.WebSocket;
-import org.java_websocket.framing.Framedata;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
-import sun.rmi.runtime.Log;
 
-/**
- *
- * @author Robert Appelmann
- */
-public class CubeSocketServer extends WebSocketServer{
+public class CubeSocketServer extends WebSocketServer {
 
-    // Fields
-    private static final int MAX_CLIENTS = 4;
+    // TODO: Identify and separate clients and servers
+    // 
     
-    // Connections
-    private WebSocket gameServer;
-    private ArrayList<WebSocket> clients;
-    
-    public CubeSocketServer() throws UnknownHostException{
-        super(new InetSocketAddress(8080));
-        this.clients = new ArrayList<>();
-    }
-    
-    @Override
-    public void onOpen(WebSocket ws, ClientHandshake ch) {
-        // What to do on client connect
+    public CubeSocketServer(InetSocketAddress address) {
+        super(address);
     }
 
     @Override
-    public void onClose(WebSocket ws, int i, String string, boolean bln) {
-        // what to do on client disconnect
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+        System.out.println("new connection to " + conn.getRemoteSocketAddress());
+        System.out.println(handshake);
     }
 
     @Override
-    public void onMessage(WebSocket ws, String string) {
-        //what to do on client message
-    }
-    
-    @Override
-    public void onMessage(WebSocket ws, ByteBuffer string) {
-        //what to do on client message
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        System.out.println("closed " + conn.getRemoteSocketAddress() + " with exit code " + code + " additional info: " + reason);
     }
 
     @Override
-    public void onError(WebSocket ws, Exception excptn) {
-        // what to do on error
-        System.out.println("Exception with " + ws.toString() + ".\n" + excptn.getMessage());
+    public void onMessage(WebSocket conn, String message) {
+        System.out.println("received message from " + conn.getRemoteSocketAddress() + ": " + message);
     }
-    
+
     @Override
-    public void onFragment(WebSocket conn, Framedata fragment)
-    {
-        // what to do on fragment receipt
+    public void onError(WebSocket conn, Exception ex) {
+        System.err.println("an error occured on connection " + conn.getRemoteSocketAddress()  + ":" + ex);
     }
-    
-    public void sendToAll(String message)
-    {
-        Collection<WebSocket> con = connections();
-        synchronized (con)
-        {
-            for(WebSocket c : con)
-            {
-                c.send(message);
-            }
-        }
-    }
-    
-    public void sendToAll(ByteBuffer buffer)
-    {
-        Collection<WebSocket> con = connections();
-        synchronized (con)
-        {
-            for(WebSocket c : con)
-            {
-                c.send(buffer);
-            }
-        }
-    }
-    
-    public static void main(String[] args)
-    {
-        try {
-            CubeSocketServer sever = new CubeSocketServer();
-        } catch (UnknownHostException ex) {
-            Logger.getLogger(CubeSocketServer.class.getName()).log(Level.SEVERE, null, ex);
-        }
+
+    public static void main(String[] args) {
+        String host = "localhost";
+        int port = 8887;
+
+        WebSocketServer server = new CubeSocketServer(new InetSocketAddress(host, port));
+        server.run();
     }
 }
